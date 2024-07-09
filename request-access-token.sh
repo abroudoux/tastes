8c34ea8906cbec362b6d2b56bf74538937c4b37a
arthur <arthur.broudoux@gmail.com>
2024-07-10 00:04:39 +0200

    feat: automatic restart server Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- request-access-token.sh
+++ request-access-token.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-env_file=../.env
+env_file=.env
 
 extract_value() {
     local key="$1"
@@ -10,9 +10,15 @@ extract_value() {
 client_id=$(extract_value "ENV_SPOTIFY_CLIENT_ID")
 client_secret=$(extract_value "ENV_SPOTIFY_CLIENT_SECRET")
 old_access_token=$(extract_value "ENV_SPOTIFY_ACCESS_TOKEN")
+port=$(extract_value "ENV_PORT")
 
 new_access_token=`curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
      -d "grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}" -s | jq -r .access_token`
 
 gsed -i "s/${old_access_token}/${new_access_token}/g" $env_file
+
+lsof -ti:$port | xargs kill
+
+pnpm run dev
+
